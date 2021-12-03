818d215ede37e702394fb07a983035d4f1885822
Andreyrvs <cortaesai@gmail.com>
2021-12-03 11:54:06 -0300

    Requisito 8.2 TDD

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,18 @@
-const fetchProducts = () => {
-  // seu código aqui
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
-  return url;
+const fetchProducts = async (param) => {
+  // seu código aqui
+  const url = `${API_URL}${param}`;
+
+  const promise = await fetch(url)
+  .then((response) => response.json())
+  .then((objJson) => objJson.results)
+  .catch((error) => error.toString());
+  
+  return promise;
 };
+
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,10 +5,15 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
   // fail('Teste vazio');
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
 
   it('é uma funcão', () => {
     const expected = typeof fetchProducts;
-    const atual = 'function'
-    expect(expected).toBe(atual)
-  })
+    const atual = 'function';
+    expect(expected).toBe(atual);
+  });
+  it('é chamado ao passar com o argumento "computador" e testa se "fetch" foi chamado', () => {
+    fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled();
+  });
 });
