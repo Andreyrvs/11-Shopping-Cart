56b29224458337b1cbd8f428a1c4a3d738e1dee1
Andreyrvs <cortaesai@gmail.com>
2021-12-03 11:01:56 -0300

    Requisito 8.1 TDD

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
 const fetchProducts = () => {
   // seu código aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+  return url;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,11 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+
+  it('é uma funcão', () => {
+    const expected = typeof fetchProducts;
+    const atual = 'function'
+    expect(expected).toBe(atual)
+  })
 });
