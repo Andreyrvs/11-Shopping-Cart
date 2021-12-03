e60c777a1903f8402decfb754c44eacc76308326
Andreyrvs <cortaesai@gmail.com>
2021-12-03 12:56:12 -0300

    Requisito 8 Finalizado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -12,26 +12,29 @@ describe('1 - Teste a função fecthProducts', () => {
     const atual = 'function';
     expect(expected).toBe(atual);
   });
+
   it('ao passar com o argumento "computador" e testa se "fetch" foi chamado', () => {
     // Falso Positivo?
     fetchProducts('computador')
     expect(fetch).toHaveBeenCalled();
   });
+
   it('ao passar com o argumento "computador", a função fetch utiliza o endpoint "url"', () => {
     fetchProducts('computador')
     expect(fetch).toHaveBeenCalledWith(url);
   });
+
   it('retorno da fetchProducts com argumento "computador" é igual ao objeto "computadorSearch"', async () => {
     const expected = await fetchProducts('computador');
     const atual = computadorSearch;
     
     expect(expected).toEqual(atual)
   });
+  
   it('sem argumento retorna um erro com a mensagem: You must provide an url', async () => {
     const expected = await fetchProducts()
     const atual = new Error('You must provide an url')
     expect(expected).toEqual(atual)
   })
 
-  // You must provide an url
 });
