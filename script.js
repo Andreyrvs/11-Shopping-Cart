fd347f6487a7671ec00e9838f7fa6b16a56e0d92
Andreyrvs <cortaesai@gmail.com>
2021-12-04 14:58:55 -0300

    Requisito 02 Finalizado

--- script.js
+++ script.js
@@ -21,32 +21,27 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.addEventListener('click', addItemCart);
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-// apagar depois
-getSkuFromProductItem();
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-  // apagar deppois
-  return event;
+  
 }
 
 // Requisito 02
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-// apagar depois
-createCartItemElement();
 
 // Requisito 01 - Lista os items no HTML
 fetchProducts('computador').then((resposta) => {
@@ -58,12 +53,18 @@ fetchProducts('computador').then((resposta) => {
 });
 
 // Requisito 02 - criar o carrinho
-// fetchItem('MLB1341706310')
-// .then((respostas) => {
+const cartOl = document.querySelector('.cart__items');
 
-// })
+const addItemCart = (event) => {
+  if (event.target.className === 'item__add') {
+    fetchItem(getSkuFromProductItem(event.path[1]))
+    .then(({ id, title, price }) => {
+      cartOl.appendChild(createCartItemElement({ id, title, price }));
+    });  
+  }
+};
 
 window.onload = () => {
-  fetchProducts('computador');
-  fetchItem('MLB1341706310');
+  fetchProducts();
+  // fetchItem();
 };
